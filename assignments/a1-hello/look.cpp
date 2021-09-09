46ddea7ee4881471dece1e5e1f84593bde8d1f77
Zoeybiulala <1064271634@qq.com>
2021-09-09 15:36:29 -0400

    a moving target!

--- assignments/a1-hello/look.cpp
+++ assignments/a1-hello/look.cpp
@@ -20,7 +20,11 @@ class Look : public atkui::Framework {
 
   virtual void scene() {
     //the target setup
-    vec3 target = vec3(_mouseX, _mouseY, 0);
+    if(mouseIsDown(GLFW_MOUSE_BUTTON_LEFT)) {
+      target = vec3(_mouseX, _mouseY, 0);
+    } else {
+      target = vec3(width() * 0.5, height() * 0.5, 0);
+    }
     setColor(vec3(1,0,0));
     drawSphere(target, 10);
 
@@ -31,33 +35,31 @@ class Look : public atkui::Framework {
     // draw the left pupil
     setColor(vec3(0,0,0));
     //the x position of the pupil move in time 
-    px1 = x1 + routeleng * sin(elapsedTime()); //TODO: update this to follow the mouse
+    px1 = x1; //TODO: update this to follow the mouse
     drawSphere(vec3(px1,y,0),30);
 
     //draw the right eye
     setColor(vec3(1,1,1));
     drawSphere(vec3(x2,y,-1000), r);
 
-    //the x position of the pupil move in time 
-    float px2 = x2 + routeleng * sin(elapsedTime());
+    //draw the right pupil
+    setColor(vec3(0,0,0));
+    px2 = x2;
     drawSphere(vec3(px2,y,0),30);
   }
 
-  void mouseMove(int x, int y) {
+  void mouseMotion(int x, int y, int dx, int dy) {
     _mouseX = x;
     _mouseY = height() - y; //?
   }
 
-  void mouseDown(int button, int mods){
-    if(button == GLFW_MOUSE_BUTTON_LEFT) {
-      
-    }
-  }
-
  private:
+  //current mouse position?
   int _mouseX;
   int _mouseY;
   float x1,y, x2,r,px1,px2;
+  vec3 target;
+  
 };
 
 int main(int argc, char** argv) {
