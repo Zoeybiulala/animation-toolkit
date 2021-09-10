318e80d151a4c66bff9637863a27c59190bb72c7
Zoeyabiubiu <1064271634@qq.com>
2021-09-09 23:30:54 -0400

    look completed!

--- assignments/a1-hello/look.cpp
+++ assignments/a1-hello/look.cpp
@@ -12,20 +12,26 @@ class Look : public atkui::Framework {
     _mouseX = width() * 0.5;
     _mouseY = height() * 0.5;
 
+
     x1 = width() * 0.3; //the left eye's x position
     y = height() * 0.5; 
     x2 = width() * 0.7; //the right eye's x position
     r = 120; //eye's radius
+
+    routeleng = 40; //the pupil's max move range
+
   }
 
   virtual void scene() {
     //the target setup
     if(mouseIsDown(GLFW_MOUSE_BUTTON_LEFT)) {
       target = vec3(_mouseX, _mouseY, 0);
+      setColor(vec3(1,0,0));
     } else {
-      target = vec3(width() * 0.5, height() * 0.5, 0);
+      target = vec3(0,0,0);
+      setColor(vec3(0,0,0));
     }
-    setColor(vec3(1,0,0));
+    
     drawSphere(target, 10);
 
     // draw the left eye
@@ -34,9 +40,12 @@ class Look : public atkui::Framework {
 
     // draw the left pupil
     setColor(vec3(0,0,0));
-    //the x position of the pupil move in time 
-    px1 = x1; //TODO: update this to follow the mouse
-    drawSphere(vec3(px1,y,0),30);
+
+    theta1 = atan2(target[1] - y , target[0] - x1);
+    px1 = x1 + routeleng * cos(theta1); 
+    py1 = y + routeleng * sin(theta1);
+
+    drawSphere(vec3(px1,py1,0),30);
 
     //draw the right eye
     setColor(vec3(1,1,1));
@@ -44,20 +53,26 @@ class Look : public atkui::Framework {
 
     //draw the right pupil
     setColor(vec3(0,0,0));
-    px2 = x2;
-    drawSphere(vec3(px2,y,0),30);
+
+    theta2 = atan2(target[1] - y , target[0] - x2);
+    //the x position of the pupil move in time 
+    px2 = x2 + routeleng * cos(theta2); 
+    py2 = y + routeleng * sin(theta2);
+    drawSphere(vec3(px2,py2,0),30);
   }
 
   void mouseMotion(int x, int y, int dx, int dy) {
     _mouseX = x;
-    _mouseY = height() - y; //?
+    _mouseY = height() - y; 
   }
 
  private:
   //current mouse position?
   int _mouseX;
   int _mouseY;
-  float x1,y, x2,r,px1,px2;
+  float routeleng;
+  float x1,y, x2,r,px1,py1, px2,py2;
+  float theta1, theta2;
   vec3 target;
   
 };
