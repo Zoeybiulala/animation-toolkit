d74a177ab1f98435306b95d634b591598f73d3ca
Zoeyabiubiu <1064271634@qq.com>
2021-09-14 21:29:51 -0400

    unique half thru

--- assignments/a1-hello/unique.cpp
+++ assignments/a1-hello/unique.cpp
@@ -8,12 +8,23 @@ class Unique : public atkui::Framework {
   }
 
   virtual void setup() {
-    // your code here!
+    theta = 0.0;
+    thetaRate = 0.2;
+    r = 100.0;
   }
 
   virtual void scene() {
-    // your code here!
+    theta += thetaRate * dt();
+    float px = r * cos(theta) + 0.5 * width();
+    float py = r * sin(theta) + 0.5 * height();
+    setColor(vec3(1, 0, 0));
+    drawSphere(vec3(px, py, 0), 100);
   }
+
+  private:
+    float theta;
+    float thetaRate;
+    float r;
 };
 
 int main(int argc, char** argv) {
